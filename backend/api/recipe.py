8b7ff3bf3c822bea9089202265ffd4d52617471a
Szym R <szym.rajewski@gmail.com>
2020-01-11 01:31:37 +0100

    Add method return all recipies in HTTP response

--- backend/api/recipe.py
+++ backend/api/recipe.py
@@ -1,4 +1,16 @@
+from . import auth
 from . import db
+from . import response_handler
+from flask import Flask, jsonify, request, session, make_response, abort, Response
+
+
+def get_all_recipes(request):
+    if auth.is_token_valid(request):
+        recipes = find_all_recipes()
+        response = make_response(jsonify(recipes), 200)
+        return response_handler.create_success_response(response)
+    else:
+        return response_handler.create_failure_response(403)
 
 
 def find_all_recipes():
@@ -6,4 +18,4 @@ def find_all_recipes():
     results = database.execute(
         "SELECT r.name, description, i.name FROM ingredient i INNER JOIN recipe r ON i.recipe_id = r.id",
     ).fetchall()
-
+    
